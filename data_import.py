e9d1d0178dbbc3b8ee962714db0799c9b17922c9
Karsten <voigt@bio.uni-freiburg.de>
2024-11-15 16:51:57 +0100

    bugfixes in main

--- data_import.py
+++ data_import.py
@@ -1,8 +1,8 @@
 import pandas as pd 
-list1_df = pd.DataFrame("list1.txt")
+list1_df = pd.DataFrame("mallorca.txt")
 list1_df.to_csv('list1.csv', index=False, header=False)
 list1_df = pd.read_csv("list1.csv")
 
-list2_df = pd.DataFrame("list2.txt")
-list1_df.to_csv('list2.csv', index=False, header=False)
-list1_df = pd.read_csv("list2.csv")
+list2_df = pd.DataFrame("predator.txt")
+list2_df.to_csv('list2.csv', index=False, header=False)
+list2_df = pd.read_csv("list2.csv")
