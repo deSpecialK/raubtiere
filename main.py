3f6ecbb93825ddaf4d667ed4015397e01b8b7e7f
Karsten <voigt@bio.uni-freiburg.de>
2024-11-15 16:59:33 +0100

    hotfix of data import

--- main.py
+++ main.py
@@ -2,6 +2,19 @@
 #
 # Authors: @anne; @emil; @Konstantin; @Karsten
 
+import sys
+
+def file2array (filename=sys.argv[1]):
+    list = []
+
+    with open (filename, r, ) as file:
+        for line in file:
+            list.add(line)
+    
+    return list
+
+
+    
 
 #Funktion zum Vergleichen zweier Listen
 def compare_lists(list1, list2):
