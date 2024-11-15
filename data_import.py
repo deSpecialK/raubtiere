b14f81a74f9a80654ef9c1345ea0bccd70b91104
konstantin <konstantin.vodoturskii@gmail.com>
2024-11-15 16:44:07 +0100

    Umwandlung und Lesung  von Liste 2

--- data_import.py
+++ data_import.py
@@ -1,4 +1,8 @@
 import pandas as pd 
 list1_df = pd.DataFrame("list1.txt")
-list1_df.to_csv('my_csv.csv', index=False, header=False)
+list1_df.to_csv('list1.csv', index=False, header=False)
 list1_df = pd.read_csv("list1.csv")
+
+list2_df = pd.DataFrame("list2.txt")
+list1_df.to_csv('list2.csv', index=False, header=False)
+list1_df = pd.read_csv("list2.csv")
