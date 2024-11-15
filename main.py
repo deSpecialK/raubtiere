336b213f45e71b364e634a7625618b6b0d4d7c2c
Karsten <voigt@bio.uni-freiburg.de>
2024-11-15 16:32:04 +0100

    added comments

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 #
 # Authors: @anne; @emil; @Konstantin; @Karsten
 
+#Funktion zum Vergleichen zweier Listen
 def compare_lists(list1, list2):
+    #vergleich beide Listen und schreibt eine Liste aller Uebereinstimmungen in die Variable common
     common = list(set(list1) & set(list2))
+    #Gibt die Variable common zurueck
     return common
